8faf99ff00fd74443b11bba867af85ffdb82c850
Mark Emery <markemery@Marks-MacBook-Pro.local>
2024-10-02 21:38:52 -0400

    10_02_2138

---
+++ src/store/store.js
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import provider from './reducers/provider'
+//import tokens from './reducers/tokens'
+//import amm from './reducers/amm'
+
+export const store = configureStore({
+  reducer: {
+    provider
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false
+    })
+})
