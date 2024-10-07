c5d2f102affb9d4389d735e55d28cd3c73251ba6
Mark Emery <markemery@Marks-MacBook-Pro.local>
2024-10-07 16:27:17 -0400

    10_07_16:27

--- src/components/Swap.js
+++ src/components/Swap.js
@@ -1,9 +1,92 @@
+import { useSelector } from 'react-redux'
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+
 const Swap = () => {
-
-                 
-
+  const account = useSelector(state => state.provider.account)
   return (
- 	<div>Swap</div>
+    <div>
+      <Card style={{ maxWidth: '450px' }} className='mx-auto px-4'>
+        {account ? (
+          <Form style= {{ maxWidth: '450px', margin: '50px auto' }}>
+            <Row className='my-3'>
+              <div className="d-flex justify-content-between">
+                <Form.Label><strong>Input:</strong></Form.Label>
+                <Form.Text muted>
+                  Balance:
+                </Form.Text>
+              </div>
+
+              <InputGroup>
+                <Form.Control
+                  type="number"
+                  placeholder="0.0"
+                  min="0.0"
+                  step="any"
+                  value={0}
+                  disabled={false}
+                />
+              
+              <DropdownButton
+                variant="outline-secondary"
+                title="Select Token"
+              >
+                <Dropdown.Item>DAPP</Dropdown.Item>
+                <Dropdown.Item>USD</Dropdown.Item>
+              </DropdownButton>
+              </InputGroup>
+            </Row>
+
+            <Row className='my-4'>
+              <div className="d-flex justify-content-between">
+                <Form.Label><strong>Output:</strong></Form.Label>
+                <Form.Text muted>
+                  Balance:
+                </Form.Text>
+              </div>
+
+              <InputGroup>
+              <Form.Control
+                type="number"
+                placeholder="0.0"
+                value={0}
+                disabled
+              />
+              <DropdownButton
+                variant="outline-secondary"
+                title="Select Token"
+              >
+                <Dropdown.Item>DAPP</Dropdown.Item>
+                <Dropdown.Item>USD</Dropdown.Item>
+              </DropdownButton>
+              </InputGroup>
+            </Row>
+
+            <Row className='my-3'>
+              <Button type='submit'>Swap</Button>
+              <Form.Text muted>
+                Exchange Rate:
+              </Form.Text>
+
+            </Row>
+
+          </Form>
+        ) : (
+          <p
+            className='d-flex justify-content-center align-items-center'
+            style={{ height: '300px' }}
+          >
+
+          Please connect wallet.
+          </p>
+        )}
+      </Card>
+    </div>
   );
 }
 
