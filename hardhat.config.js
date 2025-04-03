4bb5e8eef2de1ea952cf70fbd20612bef87ef616
memery1446 <memery1446@gmail.com>
2025-04-03 09:22:43 -0400

    operational

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 1337, // ✅ force it here
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 1337, // ✅ also here
+    },
+  },
 };
